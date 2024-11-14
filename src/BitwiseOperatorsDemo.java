30301c1138e42775879110dc13ed981ad9f096d2
sir-eye <ial-reserve@live.com>
2024-11-14 13:26:11 -0700

    Core Java: Operators.

---
+++ src/BitwiseOperatorsDemo.java
@@ -0,0 +1,13 @@
+public class BitwiseOperatorsDemo {
+    public static void main(String[] args) {
+
+        int x = 58;
+        int y = 13;
+        System.out.println("x & y : " + (x & y));
+        System.out.println("x | y : " + (x | y));
+        System.out.println("x ^ y : " + (x ^ y));
+        System.out.println("~x : " + (~x));
+        System.out.println("x << y : " + (x << y));
+        System.out.println("x >> y : " + (x >> y));
+    }
+}
